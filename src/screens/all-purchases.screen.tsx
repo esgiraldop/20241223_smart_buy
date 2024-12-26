09dc16d6bc29f65e90867b709ac3a909da57c627
Erick Giraldo <erickgiraldo94@gmail.com>
2024-12-26 18:10:39 -0500

    feat: first version of all purchases screen

--- src/screens/all-purchases.screen.tsx
+++ src/screens/all-purchases.screen.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import {Button, Text, View} from 'react-native';
 import {RootStackParamList} from '../interfaces';
 import {useNavigation} from '@react-navigation/native';
+import {usePurchases} from '../hooks/use-purchases.hook';
+import {textStyles} from '../styles';
 
 export type AllPurchasesScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -12,19 +14,40 @@ export type AllPurchasesScreenNavigationProp = NativeStackNavigationProp<
 export const AllPurchasesScreen = () => {
   const navigation = useNavigation<AllPurchasesScreenNavigationProp>();
 
+  const {purchases, errorLoadingPurchases, isPurchaseLoading} = usePurchases();
+
   return (
-    <View>
-      <Text style={{color: 'black'}}>
-        This is the screen for viewing all the purchases
-      </Text>
-      <Button
-        onPress={() => navigation.navigate('CreatePurchase')}
-        title="Go to create purchase"
-      />
-      <Button
-        onPress={() => navigation.navigate('EditPurchase')}
-        title="Go to edit purchase"
-      />
-    </View>
+    <>
+      {isPurchaseLoading ? (
+        // <Loader />
+        <Text style={textStyles.textSuccess}>Loading purchases...</Text>
+      ) : errorLoadingPurchases ? (
+        <Text style={textStyles.textError}>
+          Error loading budgets and transactions
+        </Text>
+      ) : purchases.length < 1 ? (
+        <>
+          <Text style={{color: 'black'}}>No Purchases could be loaded</Text>
+          <Button
+            onPress={() => navigation.navigate('CreatePurchase')}
+            title="Go to create purchase"
+          />
+        </>
+      ) : (
+        <View>
+          <Text style={{color: 'black'}}>
+            This is the screen for viewing all the purchases
+          </Text>
+          <Button
+            onPress={() => navigation.navigate('CreatePurchase')}
+            title="Go to create purchase"
+          />
+          <Button
+            onPress={() => navigation.navigate('EditPurchase')}
+            title="Go to edit purchase"
+          />
+        </View>
+      )}
+    </>
   );
 };
